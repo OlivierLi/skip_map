0ef855814f006696a824407b05873609bc689d8c
olivierli <olivierlidupuis@gmail.com>
2019-09-18 10:54:37 -0400

    Add comments. Inhibit optimizations

--- src/events.cpp
+++ src/events.cpp
@@ -1,31 +1,44 @@
 /// This executable is intended as a simple way to execute precisely the code we
 /// want to inspect using Linux Perf.
 
+// See cycles difference  : sudo perf record -e cycles:u -g -- ./events
+// See misses difference  : sudo perf record -e LLC-misses -c 10 -g -- ./events
+
+// Analyze using : sudo perf report --symbol-filter=iterate
+
 #include <iostream>
 #include <list>
+#include "benchmark/benchmark.h"
 #include "skip_map.h"
 #include "test_facilities.hpp"
 
 size_t large_size = 10000;
 
-void iterate(skip_map<Key, Value>& sm) {
-  for (auto it = sm.begin(); it != sm.end(); ++it)
-    ;
+void __attribute__((noinline)) iterateSkipMap(skip_map<Key, Value>& sm) {
+  for (auto it = sm.begin(); it != sm.end();) {
+    benchmark::DoNotOptimize(++it);
+  }
 }
 
-int main(int, char**) {
-  // Create a list.
-  std::list<KeyValue> list(large_size, {1, long_string});
+void __attribute__((noinline))
+iterateList(std::list<std::pair<Key, Value>>& list) {
+  for (auto it = list.begin(); it != list.end();)
+    benchmark::DoNotOptimize(++it);
+}
+
+int main(int argc, char**) {
+  // Create a list. Initiliaze key to argc to prevent some optimizations maybe.
+  std::list<KeyValue> list(large_size, {argc, long_string});
 
   // Create an equivalent skip_map.
   skip_map<Key, Value> sm;
   sm.set_gen_for_testing([]() { return 0; });
   fill(sm, large_size);
 
-  for (auto it = sm.begin(); it != sm.end(); ++it)
-    ;
-  for (auto it = list.begin(); it != list.end(); ++it)
-    ;
+  for (int i = 0; i < 1000; ++i) {
+    iterateSkipMap(sm);
+    iterateList(list);
+  }
 
-  return list.size();
+  return 0;
 }
